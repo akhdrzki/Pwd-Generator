a70ed8c8afe6dbf1f7044480fd8bd5ee46ec4e77
QRZk <87889949+akhadirizki@users.noreply.github.com>
2022-11-04 16:10:39 +0800

    Add files via upload

---
+++ Password Generator/pwgen.py
@@ -0,0 +1,21 @@
+import random
+
+print("Password Generator")
+
+chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*?.,()123456789"
+
+num = input("Amount of your password to generate: ")
+
+num = int(num)
+
+length = input("Input your password length: ")
+
+length = int(length)
+
+print("there are your passwords")
+
+for pwd in range(num):
+	passwords = ""
+	for c in range(length):
+		passwords += random.choice(chars)
+	print(passwords)
